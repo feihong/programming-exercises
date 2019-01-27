0bc2961a5fdb9a094a9884b7cb46290624429c4a
Feihong Hsu <feihong.hsu@gmail.com>
2019-01-26 23:53:21 -0600

    Add is_palindrome

--- src/99/lists_test.ml
+++ src/99/lists_test.ml
@@ -5,13 +5,16 @@ let expect_all xs =
   let (a, b) = xs |> List.toArray |> Array.unzip in
   let open Expect in  expect a |> toEqual b
 
+(* 1 *)
 let rec last = function [] -> None | [x] -> Some x | _x :: xs -> last xs
 
+(* 2 *)
 let rec last_two = function
 | [] | [_] -> None
 | [a; b] -> Some (a, b)
 | _ :: xs -> last_two xs
 
+(* 3 *)
 let rec at n = function
 | [] -> None
 | x :: xs ->
@@ -20,14 +23,30 @@ let rec at n = function
   | n when n < 1 -> None
   | n -> at (n - 1) xs
 
+(* 4 *)
 let length xs =
   let rec go acc = function [] -> acc | _ :: xs -> go (acc + 1) xs in
   go 0 xs
 
+(* 5 *)
 let reverse xs =
   let rec go acc = function [] -> acc | x :: xs -> go (x :: acc) xs in
   go [] xs
 
+(* 6 *)
+let is_palindrome xs = xs = reverse xs
+
+(* 7 *)
+(* There is no nested list type in OCaml, so we need to define one
+     first. A node of a nested list is either an element, or a list of
+     nodes. *)
+type 'a node =
+  | One of 'a
+  | Many of 'a node list
+
+
+
+(* Tests *)
 let () = describe "Lists" @@ fun () ->
 
 test "last" (fun () ->
@@ -64,4 +83,12 @@ test "reverse" (fun () ->
     reverse [], [];
     reverse [1], [1];
     reverse [1;2;3], [3;2;1];
-  ])
+  ]);
+
+test "is_palindrome" (fun () ->
+  expect_all [
+    is_palindrome [], true;
+    is_palindrome [1], true;
+    is_palindrome [1;2], false;
+    is_palindrome [1;2;3;2;1], true;
+  ]);
