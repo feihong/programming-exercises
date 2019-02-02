6d04e78953e9eec31218c88557efb19f6e143149
Feihong Hsu <feihong.hsu@gmail.com>
2019-02-02 10:02:30 -0600

    Slightly improved rand_select

--- src/99/lists_21_28_test.ml
+++ src/99/lists_21_28_test.ml
@@ -20,9 +20,9 @@ let range i k =
 (* 23 *)
 let rand_select lst n =
   let rec remove i acc = function
-  | [] -> (0, [])  (* should be unreachable *)
+  | [] -> raise Not_found
   | h :: t ->
-    if i = 0 then (h, List.reverse acc @ t)
+    if i = 0 then (h, acc @ t)
     else remove (i - 1) (h :: acc) t
   in
   let rec aux acc_len acc lst_len = function
@@ -72,11 +72,11 @@ test "rand_select" (fun () ->
     rand_select [] 5, [];
     rand_select [1;2;3] 1, [1];
     rand_select [1;2;3] 2, [1;2];
-    rand_select [1;2;3] 3, [1;2;3];
     rand_select [1;2;3] 3, [2;1;3];
-    rand_select [1;2;3;4;5;6;7] 3, [1;4;5];
-    rand_select [1;2;3;4;5;6;7] 3, [2;1;6];
-    rand_select [1;2;3;4;5;6;7] 3, [2;4;5];
-    rand_select [1;2;3;4;5;6;7] 3, [6;7;3];
+    rand_select [1;2;3] 3, [1;2;3];
+    rand_select [1;2;3;4;5;6;7] 3, [2;1;5];
+    rand_select [1;2;3;4;5;6;7] 3, [4;5;6];
+    rand_select [1;2;3;4;5;6;7] 3, [3;1;5];
+    rand_select [1;2;3;4;5;6;7] 3, [2;7;3];
     rand_select [1;2;3;4;5;6;7] 3, [6;1;2];
   ]);
